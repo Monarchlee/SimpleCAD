2e6a0813a3fabed731aa629854955f338ee62935
UPiracy <upiracy@163.com>
2020-09-28 20:17:04 +0800

    更新了Volume

--- Assets/Basics/Volume.cs
+++ Assets/Basics/Volume.cs
@@ -1,92 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Volume
 {
-    public abstract class Volume
-    {
-        /// <summary>
-        /// 展示箱的大小
-        /// </summary>
-        public Vector3 Scale;
-        /// <summary>
-        /// 数据储存步数
-        /// </summary>
-        public Vector3Int StorageSteps;
-
-        public long Size => (StorageSteps.x + 1) * (StorageSteps.y + 1) * (StorageSteps.z + 1);
-
-        public virtual float this[Vector3Int v]
-        {
-            get { return 0; }
-            set { }
-        }
-
-    }
-
-    public class LogicalVolume : Volume
+    [System.Serializable]
+    public class Volume
     {
         /// <summary>
         /// 储存体积
         /// </summary>
-        byte[] volume;
+        byte[] data;
 
-        public LogicalVolume(Vector3 scale, Vector3Int storageSteps)
+        /// <summary>
+        /// 展示箱的大小
+        /// </summary>
+        public Vector3 Size;
+        /// <summary>
+        /// 数据储存步数
+        /// </summary>
+        public Vector3Int Density
         {
-            Scale = scale;
-            StorageSteps = storageSteps;
-            long length = Size / 8 + (((Size % 8) == 0) ? 0 : 1);
-            volume = new byte[length];
+            get { return density; }
+            set
+            {
+                density = value;
+            }
         }
-        
-        public override float this[Vector3Int v]
+        Vector3Int density;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="size">展示箱的大小</param>
+        /// <param name="density">密度(步数)</param>
+        public Volume(Vector3 size, Vector3Int density)
+        {
+            Size = size;
+            Density = density;
+            long length = Length / 8 + (((Length % 8) == 0) ? 0 : 1);
+            data = new byte[length];
+        }
+
+        public long Length => (Density.x + 1) * (Density.y + 1) * (Density.z + 1);
+
+        public float this[Vector3Int v]
         {
             get
             {
-                long index = (v.z * (StorageSteps.z + 1) + v.y) * (StorageSteps.y + 1) + v.x;
-                byte target = volume[index / 8];
+                long index = (v.z * (Density.z + 1) + v.y) * (Density.y + 1) + v.x;
+                byte target = data[index / 8];
                 if ((target & (1 << (int)(index % 8))) == 0) return 0;
                 else return 1;
             }
             set
             {
-                long index = (v.z * (StorageSteps.z + 1) + v.y) * (StorageSteps.y + 1) + v.x;
-                byte target = volume[index / 8];
+                long index = (v.z * (Density.z + 1) + v.y) * (Density.y + 1) + v.x;
+                byte target = data[index / 8];
                 int insert = value > 0 ? 1 : 0;
                 target &= (byte)(insert << (int)(index % 8));
-                volume[index / 8] = target;
+                data[index / 8] = target;
             }
         }
-    }
 
-    public class DensityVolume : Volume
-    {
-        /// <summary>
-        /// 储存体积
-        /// </summary>
-        byte[,,] volume;
-
-        public DensityVolume(Vector3 scale, Vector3Int storageSteps)
+        public static void Serialize(Volume v, string path)
         {
-            Scale = scale;
-            StorageSteps = storageSteps;
-            volume = new byte[storageSteps.x, storageSteps.y, storageSteps.z];
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, v);
+                fs.Close();
+                fs.Dispose();
+            }
         }
 
-        public override float this[Vector3Int v]
+        public static Volume Deserialize(string path)
         {
-            get
+            Volume v;
+            using (FileStream fs = new FileStream(path, FileMode.Open))
             {
-                byte target = volume[v.x, v.y, v.z];
-                target -= 128;
-                return (float)target / 128;
+                BinaryFormatter bf = new BinaryFormatter();
+                v = (Volume)bf.Deserialize(fs);
+                fs.Close();
+                fs.Dispose();
             }
-            set
-            {
-                byte target = (byte)(value * 128);
-                volume[v.x, v.y, v.z] = target;
-            }
+            return v;
         }
     }
 }
