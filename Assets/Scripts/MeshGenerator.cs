386d8cb3eec018ef3db25378c8e47510731af934
UPiracy <upiracy@163.com>
2020-10-08 23:13:32 +0800

    更新了一套能跑但有Bug的March
    目前的测试结果：当三角形数有限时效果正常
    当三角形数提高时，输出Debug的三角形个数并没有上限，但显示明显失常，考虑是march或load的过程出问题

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VolumeData;
+
+public class MeshGenerator : MonoBehaviour
+{
+    [SerializeField] ComputeShader march = null;
+    [SerializeField] ComputeShader transfer = null;
+    [SerializeField] Material mat = null;
+
+    [SerializeField] float isoLevel = 0;
+
+    ComputeBuffer triangleBuffer;
+    ComputeBuffer pointsBuffer;
+    ComputeBuffer triCountBuffer;
+
+    public void WriteData(Volume volume)
+    {
+        Vector3Int threadCount = volume.VoxelThreadCount;
+
+        ComputeBuffer dataBuffer = new ComputeBuffer(volume.SamplesCount, sizeof(float));
+        transfer.SetInt("numPointsX", volume.SamplesDensity.x);
+        transfer.SetInt("numPointsY", volume.SamplesDensity.y);
+        transfer.SetInt("numPointsZ", volume.SamplesDensity.z);
+        transfer.SetVector("cellSize", volume.VoxelSize);
+        transfer.SetBuffer(1, "datas", dataBuffer);
+        transfer.SetBuffer(1, "points", pointsBuffer);
+
+        transfer.Dispatch(1, threadCount.x, threadCount.y, threadCount.z);
+
+        dataBuffer.GetData(volume.data);
+        //理论上,pointsBuffer中的数据转移到了volume中
+        dataBuffer.Dispose();
+    }
+
+    public void ReadData(Volume volume)
+    {
+        triCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
+        pointsBuffer = new ComputeBuffer(volume.SamplesCount, sizeof(float) * 4);
+        triangleBuffer = new ComputeBuffer(volume.VoxelCount * 5, sizeof(float) * 3 * 3, ComputeBufferType.Append);
+
+        Vector3Int threadCount = volume.SamplesThreadCount;
+
+        ComputeBuffer dataBuffer = new ComputeBuffer(volume.SamplesCount, sizeof(float));
+        transfer.SetInt("numPointsX", volume.SamplesDensity.x);
+        transfer.SetInt("numPointsY", volume.SamplesDensity.y);
+        transfer.SetInt("numPointsZ", volume.SamplesDensity.z);
+        transfer.SetVector("cellSize", volume.VoxelSize);
+        dataBuffer.SetData(volume.data);
+        transfer.SetBuffer(0, "datas", dataBuffer);
+        transfer.SetBuffer(0, "points", pointsBuffer);
+        transfer.Dispatch(0, threadCount.x, threadCount.y, threadCount.z);
+        //理论上,pointsBuffer已经设置完毕
+
+        dataBuffer.Dispose();
+    }
+
+    public Mesh GenerateMesh(Volume volume)
+    {
+        Mesh mesh = new Mesh();
+
+        Vector3Int threadCount = volume.VoxelThreadCount;
+
+        triangleBuffer.SetCounterValue(0);
+        march.SetBuffer(0, "points", pointsBuffer);
+        march.SetBuffer(0, "triangles", triangleBuffer);
+        march.SetInt("numPointsX", volume.SamplesDensity.x);
+        march.SetInt("numPointsY", volume.SamplesDensity.y);
+        march.SetInt("numPointsZ", volume.SamplesDensity.z);
+        march.SetFloat("isoLevel", isoLevel);
+
+        march.Dispatch(0, threadCount.x, threadCount.y, threadCount.z);
+
+        int[] triCountArray = { 0 };
+        ComputeBuffer.CopyCount(triangleBuffer, triCountBuffer, 0);
+        triCountBuffer.GetData(triCountArray);
+        int numTris = triCountArray[0];
+        Debug.Log("Triangles:" + numTris + "/" + volume.VoxelCount * 5);
+
+        Triangle[] tris = new Triangle[numTris];
+        triangleBuffer.GetData(tris, 0, 0, numTris);
+
+        var vertices = new Vector3[numTris * 3];
+        var meshTriangles = new int[numTris * 3];
+
+        for (int i = 0; i < numTris; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                meshTriangles[i * 3 + j] = i * 3 + j;
+                vertices[i * 3 + j] = tris[i][j];
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.triangles = meshTriangles;
+
+        mesh.RecalculateNormals();
+
+        return mesh;
+    }
+
+    public void UnloadBuffer()
+    {
+        triangleBuffer.Dispose();
+        pointsBuffer.Dispose();
+        triCountBuffer.Dispose();
+    }
+
+    private void Start()
+    {
+        Volume sphere = new Volume();
+        ReadData(sphere);
+        Mesh mesh = GenerateMesh(sphere);
+        GetComponent<MeshFilter>().mesh = mesh;
+        UnloadBuffer();
+    }
+
+}
+
+struct Triangle
+{
+#pragma warning disable 649 // disable unassigned variable warning
+    public Vector3 a;
+    public Vector3 b;
+    public Vector3 c;
+
+    public Vector3 this[int i]
+    {
+        get
+        {
+            switch (i)
+            {
+                case 0:
+                    return a;
+                case 1:
+                    return b;
+                default:
+                    return c;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Volume.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace VolumeData
+{
+    [System.Serializable]
+    public class Volume
+    {
+        #region 可修改的参数和数据
+        /// <summary>
+        /// 储存体积
+        /// </summary>
+        public float[] data;
+
+        /// <summary>
+        /// 展示箱的大小
+        /// </summary>
+        public Vector3 Size;
+        /// <summary>
+        /// 每个方向上点的个数
+        /// </summary>
+        public Vector3Int SamplesDensity
+        {
+            get { return samplesDensity; }
+            set
+            {
+                samplesDensity = value;
+            }
+        }
+        Vector3Int samplesDensity;
+        #endregion
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="size">展示箱的大小</param>
+        /// <param name="density">密度(步数)</param>
+        public Volume(Vector3 size, Vector3Int density)
+        {
+            Size = size;
+            SamplesDensity = density;
+            data = new float[SamplesCount];
+        }
+        /// <summary>
+        /// 这个构造函数用于返回一个球体
+        /// </summary>
+        public Volume()
+        {
+            Size = Vector3.one;
+            SamplesDensity = Vector3Int.one * 200;
+            Vector3 center = Vector3.one * 99.5f;
+            data = new float[SamplesCount];
+            for(int j = 0; j < SamplesDensity.y; j++)
+            {
+                for(int i = 0; i < SamplesDensity.x; i++)
+                {
+                    for(int k = 0; k < SamplesDensity.z; k++)
+                    {
+                        Vector3Int v = new Vector3Int(i, j, k);
+                        this[v] = 1000 - (v - center).sqrMagnitude;
+                    }
+                }
+            }
+        }
+
+        #region 属性
+        /// <summary>
+        /// 每个方向上Cube的个数
+        /// </summary>
+        public Vector3Int VoxelsDensity => SamplesDensity - Vector3Int.one;
+        /// <summary>
+        /// 单个Cube的大小
+        /// </summary>
+        public Vector3 VoxelSize => new Vector3(Size.x / (float)VoxelsDensity.x, Size.y / (float)VoxelsDensity.y, Size.z / (float)VoxelsDensity.z);
+        /// <summary>
+        /// 点的总个数
+        /// </summary>
+        public int SamplesCount => SamplesDensity.x * SamplesDensity.y * SamplesDensity.z;
+        /// <summary>
+        /// Cube的总个数
+        /// </summary>
+        public int VoxelCount => VoxelsDensity.x * VoxelsDensity.y * VoxelsDensity.z;
+        /// <summary>
+        /// 每个方向上线程的个数
+        /// </summary>
+        public Vector3Int VoxelThreadCount => new Vector3Int(Mathf.CeilToInt((float)VoxelsDensity.x / 8), Mathf.CeilToInt((float)VoxelsDensity.y / 8), Mathf.CeilToInt((float)VoxelsDensity.z / 8));
+        /// <summary>
+        /// 每个方向上线程的个数
+        /// </summary>
+        public Vector3Int SamplesThreadCount => new Vector3Int(Mathf.CeilToInt((float)SamplesDensity.x / 8), Mathf.CeilToInt((float)SamplesDensity.y / 8), Mathf.CeilToInt((float)SamplesDensity.z / 8));
+        #endregion
+
+        private int GetIndex(Vector3Int v)
+        {
+            return (v.y * SamplesDensity.y + v.z) * SamplesDensity.z + v.x;
+        }
+
+        /// <summary>
+        /// 获得某点的密度
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public float this[Vector3Int v]
+        {
+            get
+            {
+                if (v.x >= SamplesDensity.x || v.y >= SamplesDensity.y || v.z >= SamplesDensity.z) throw new System.IndexOutOfRangeException();
+                int index = GetIndex(v);
+                return data[index];
+            }
+            set
+            {
+                if (v.x >= SamplesDensity.x || v.y >= SamplesDensity.y || v.z >= SamplesDensity.z) throw new System.IndexOutOfRangeException();
+                int index = GetIndex(v);
+                data[index] = value;
+            }
+        }
+
+        public static void Serialize(Volume v, string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, v);
+                fs.Close();
+                fs.Dispose();
+            }
+        }
+
+        public static Volume Deserialize(string path)
+        {
+            Volume v;
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                v = (Volume)bf.Deserialize(fs);
+                fs.Close();
+                fs.Dispose();
+            }
+            return v;
+        }
+    }
+}
+
