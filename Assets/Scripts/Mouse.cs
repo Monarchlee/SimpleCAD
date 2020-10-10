638847be4735c4af72d90492234b959f3d6acbd1
KaraLee <44290154+Monarchlee@users.noreply.github.com>
2020-10-10 15:33:36 +0800

    笔刷指示器初步

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Mouse : MonoBehaviour
+{
+    public Camera m_Camera;
+    public Vector3 size = new Vector3(0.5f, 0.5f, 0.5f);
+    public GameObject sphere;
+    private Vector3 position = new Vector3(0, 0, 0);
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.localScale = size;
+        sphere.transform.position = position;
+        Destroy(sphere.GetComponent<SphereCollider>());
+        sphere.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = m_Camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (sphere.activeSelf == false)
+            sphere.SetActive(true);
+        if (Physics.Raycast(ray, out hit))//击中mesh
+        {
+            position = hit.point;
+            sphere.transform.position = position;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            //修改密度值
+
+        }
+    }
+}
